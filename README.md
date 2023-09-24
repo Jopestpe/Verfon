e89d8613071bdc80706e9247e97fd90e5603aaab
Jopestpe <47086979+Jopestpe@users.noreply.github.com>
2023-09-24 20:33:23 -0300

    Pequena reforma

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Verfon 
-## Verfon é um pagina para carregar e vizualiar caracteres
-### É possivel⤵️
+## Verfon é um pagina para carregar e visualizar caracteres
+### É possível⤵️
 * Carregar Fontes .otf .ttf
-* Vizualizar caracteres personalizado
-* Vizualizar paginas de caracteres
+* Visualizar caracteres personalizado
+* Visualizar paginas de caracteres
 * Alterar tamanho dos caracteres 
 * Alterar tamanho do contorno dos caracteres 
 * Trocar cor dos caracteres
 * Trocar cor do fundo dos caracteres
-* Trocar cor da outline dos caracteres
+* Trocar cor da borda dos caracteres
--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+// Elementos
 var EntradaDeArquivo = document.getElementById('EntradaDeArquivo');
 var BotaoEntradaDeArquivo = document.getElementById('BotaoEntradaDeArquivo');
-var NomeDaFonte = document.getElementById('NomeDaFonte');
+var PaginasDeCaractere = document.getElementById('PaginasDeCaractere');
+var TamanhoDosCaracteres = document.getElementById('TamanhoDosCaracteres');
+var CorDosCaracteres = document.getElementById('CorDosCaracteres');
+var CorDeFundo = document.getElementById('CorDeFundo');
+var CorDoContorno = document.getElementById('CorDoContorno');
+var TamanhoDoContorno = document.getElementById('TamanhoDoContorno');
+var Caracteres = document.getElementById('Caracteres');
+//
+var CaracteresPorPagina = 1024;
+// Inicia definindo TamanhoDosCaracteres e PaginasDeCaractere
+DefinirTamanhoDosCaracteres(TamanhoDosCaracteres.value);
+DefinirPaginasDeCaractere(PaginasDeCaractere.value);
+// Botao para entrada
+BotaoEntradaDeArquivo.addEventListener('click',()=>{
+    EntradaDeArquivo.click();
+});
+// Entrada de arquivo
 EntradaDeArquivo.addEventListener('change', () => {
     var LeitorDeDocumentos = new FileReader();
     LeitorDeDocumentos.readAsDataURL(EntradaDeArquivo.files[0]);
@@ -9,51 +26,45 @@ EntradaDeArquivo.addEventListener('change', () => {
         const FonteAtual = new FontFace("FonteAtual", `url(${FonteArquivo})`);
         FonteAtual.load().then(() =>{
             document.fonts.add(FonteAtual);
-            document.body.classList.add("fonts-loaded");
             document.getElementById("Caracteres").style.fontFamily = "FonteAtual";
         });
     }
-    FonteAtual.family;
 });
-BotaoEntradaDeArquivo.addEventListener('click',()=>{
-    EntradaDeArquivo.click();
-});
-var Caracteres = document.getElementById('Caracteres');
-var AreaDoTexto = document.getElementById('AreaDoTexto');
-AreaDoTexto.addEventListener('input',()=>{
-    Caracteres.textContent = AreaDoTexto.value
-});
-var CaracteresPorPagina = 1024;
-var PaginasDeCaractere = document.getElementById('PaginasDeCaractere');
-var Caracteres = document.getElementById('Caracteres');
+//
 PaginasDeCaractere.addEventListener('input', () => {
-    const Inicio = (parseInt(PaginasDeCaractere.value) - 1) * CaracteresPorPagina + 1;
-    const Fim = parseInt(PaginasDeCaractere.value) * CaracteresPorPagina;
-    var Texto = ""; 
+    DefinirPaginasDeCaractere(PaginasDeCaractere.value);
+    
+});
+//
+function DefinirPaginasDeCaractere(Pagina){
+    const Inicio = (parseInt(Pagina) - 1) * CaracteresPorPagina + 1;
+    const Fim = parseInt(Pagina) * CaracteresPorPagina;
+    Caracteres.value = ""; 
     for (let Posicao = Inicio; Posicao <= Fim; Posicao++) {
-        Texto = Texto + " " + "&#" + Posicao + ";";
+        Caracteres.value += String.fromCodePoint(0x1F + Posicao - 1);
     }
-    Caracteres.innerHTML = Texto;
-});
-var TamanhoDosCaracteres = document.getElementById('TamanhoDosCaracteres');
+}
+//
 TamanhoDosCaracteres.addEventListener('input',()=>{
-    Caracteres.style.fontSize = TamanhoDosCaracteres.value + 'px';
+    DefinirTamanhoDosCaracteres(TamanhoDosCaracteres.value);
 });
-var CorDosCaracteres = document.getElementById('CorDosCaracteres');
+//
+function DefinirTamanhoDosCaracteres(Tamanho){
+    Caracteres.style.fontSize = Tamanho + 'px';
+}
+//
 CorDosCaracteres.addEventListener('input',()=> {
   Caracteres.style.color = CorDosCaracteres.value;
 });
-var CorDeFundo = document.getElementById('CorDeFundo');
+//
 CorDeFundo.addEventListener('input',()=> {
   Caracteres.style.backgroundColor = CorDeFundo.value;
 });
-var CorDoContorno = document.getElementById('CorDoContorno');
+//
 CorDoContorno.addEventListener('input',()=> {
-  Caracteres.style.webkitTextStrokeColor = CorDoContorno.value;
+    Caracteres.style.webkitTextStrokeColor = CorDoContorno.value;
 });
-var TamanhoDoContorno = document.getElementById('TamanhoDoContorno');
+//
 TamanhoDoContorno.addEventListener('input',()=>{
     Caracteres.style.webkitTextStrokeWidth = TamanhoDoContorno.value + 'px';
 });
-TamanhoDosCaracteres.dispatchEvent(new Event('input'));
-PaginasDeCaractere.dispatchEvent(new Event('input'));
