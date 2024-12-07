58270e1165fbca912706172fbc1fcb0b537a4850
Jopestpe <47086979+Jopestpe@users.noreply.github.com>
2024-12-07 02:53:45 -0300

    Corrigindo caminho service worker

--- carregar_sw.js
+++ carregar_sw.js
@@ -1,6 +1,6 @@
 window.addEventListener('load',()=>{
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
         .then(function(reg) {
             console.log(reg.scope);
         }).catch(function(erro) {
