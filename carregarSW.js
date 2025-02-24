ff721d0d9c7234eda8331663143e7a5b5d0de32f
Jopestpe <47086979+Jopestpe@users.noreply.github.com>
2025-02-24 13:54:53 -0300

    Carregar service worker (PWA)

---
+++ carregarSW.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',()=>{
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/serviceWorker.js')
+        .then(function(reg) {
+            console.log(reg.scope);
+        }).catch(function(erro) {
+            console.log(erro);
+        });
+    }
+});
