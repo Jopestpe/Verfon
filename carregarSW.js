ade8a889dcd59648e4a455aaa5fa910b052b541b
Jopestpe <47086979+Jopestpe@users.noreply.github.com>
2025-02-24 13:58:09 -0300

    Caminho errado https://jopestpe.github.io/serviceWorker.js

--- carregarSW.js
+++ carregarSW.js
@@ -1,6 +1,6 @@
 window.addEventListener('load',()=>{
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/serviceWorker.js')
+        navigator.serviceWorker.register('serviceWorker.js')
         .then(function(reg) {
             console.log(reg.scope);
         }).catch(function(erro) {
